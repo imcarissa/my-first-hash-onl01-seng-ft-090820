05ba4478c4d8aa8225ccef8931acbf17c6d0d85f
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-17 12:59:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,10 +3,10 @@ def my_hash
 end
 
 
-def shipping_manifest(the_manifest)
+def shipping_manifest
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2,
   "oil paintings" => 3}
-end
+  end
 end
 
 def retrieval
