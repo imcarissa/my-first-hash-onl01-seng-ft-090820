20ad4852e1a801b01d1f71fa25368c83201b5f2f
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-17 13:17:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -24,7 +24,7 @@ def adding
                   "oil paintings" => 3
                  }
     shipping_manifest["muskets"] = 2
-    puts shipping_manifest
     shipping_manifest["gun powder"] = 4
-    puts shipping_manifest
+    
+    shipping_manifest
 end
